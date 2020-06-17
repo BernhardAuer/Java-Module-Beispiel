2db568fb62b3c4ef6594e980cc5f3b3c347232e7
Bernhard Auer <auer.bernie@gmail.com>
2020-06-17 16:54:21 +0200

    update application (use new factory)

--- consumermodule/src/main/java/com/mci/consumermodule/Application.java
+++ consumermodule/src/main/java/com/mci/consumermodule/Application.java
@@ -1,11 +1,12 @@
 package com.mci.consumermodule;
 
-import com.mci.servicemodule.LowercaseTextService;
-import com.mci.servicemodule.TextService;
+
+import com.mci.servicemodule.external.TextService;
+import com.mci.servicemodule.external.TextServiceFactory;
 
 public class Application {
     public static void main(String args[]) {
-        TextService textService = new LowercaseTextService();
+        TextService textService = TextServiceFactory.getTextService("lowercase");
         System.out.println(textService.processText("Hello World!"));
     }
 }
